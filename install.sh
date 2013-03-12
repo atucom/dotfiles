fdfffdbcae149700861efb0c428cf1c42d495fb6
atucom <log1kl@gmail.com>
2013-03-11 22:27:36 -0500

    fixed typo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 source_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ $(uname) = "Darwin" ]]; then #this is for OSX Machines
-    md5_bin="openssl md5 "
+    md5_bin="md5"
 fi
 if [[ $(uname) = "Linux" ]]; then #this is for Linux
     md5_bin="md5sum"
@@ -28,18 +28,4 @@ for i in $source_path/confs/*; do
     fi
 done
 
-
-
-
-#check if it exists
-#    yes: are they the same file? ($md5_bin)
-#        yes: echo "same file, nothing done"
-#        no: backup the file to $name.bak0
-#            echo "backed up file to $name.bak0"
-#    no: link the file
-#
-#check if $HOME/scripts exists in $path
-#    yes: does the scripts/ dir contain the links?
-#        yes: echo bins already linked, nothing to do
-#        no: move the bins into the dir
-#    no: create it and link bin/ files into it
+#Special Installation Steps
