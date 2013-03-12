d66111ec9f7d6b9d7e6196a91364276565e912be
atucom <log1kl@gmail.com>
2013-03-11 20:01:20 -0500

    fixed typo

--- install.sh
+++ install.sh
@@ -7,17 +7,17 @@ if [[ $(uname) = "Darwin" ]]; then #this is for OSX Machines
     md5_bin="openssl md5 "
 fi
 if [[ $(uname) = "Linux" ]]; then #this is for Linux
-    md5_bin="md5sum | awk '{print $1}'"
+    md5_bin="md5sum"
 fi
 
 #link the files in the confs dir to dest_conf_dir
 for i in $source_path/confs/*; do 
-    basename_file=$(basename $i)
+    basename_file="$(basename $i)"
     dest_conf_dir="$HOME"
     dest_path="$dest_conf_dir/${basename_file/_/.}"
     source_file="$i"
     if [[ -f $dest_path ]]; then #if the dest_path exists then
-        if [[ $(cat $dest_path | $md5_bin) = $(cat $source_file | $md5_bin) ]]; then #if the file is the same
+        if [[ $(cat "$dest_path" | "$md5_bin") = $(cat "$source_file" | "$md5_bin") ]]; then #if the file is the same
             echo "$dest_path is already the same"
         else
             echo backing up ${dest_path} to ${dest_path}.bak
