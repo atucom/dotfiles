bd678bc3415319bce57a487f889372dfb515a0d2
atucom <log1kl@gmail.com>
2013-08-15 12:36:57 -0500

    fixed/updated expand_range script

--- bin/atu-expand_range.sh
+++ bin/atu-expand_range.sh
@@ -1,7 +1,15 @@
 #!/bin/bash    
-    if [[ -z $1 ]] || [[ -z $2 ]]; then
-        echo 'Expands nmap compatible ranges from the first file into the second file'
-        echo "Usage: $0 <CIDRFile> <OutputFile> "
+    if [[ -z $1 ]]; then
+    	cat <<USAGE
+$(basename ${0})
+Expands nmap compatible ranges to list of individual IPs
+
+Usage: $(basename ${0}) <NetworkRange>
+
+Example:
+  $(basename ${0}) 10.0.0-255.1-254
+  $(basename ${0}) 1.1.1.1/24
+USAGE
     else
-        nmap -sL -n -iL $1 | grep 'Nmap scan' | cut -f 5 > $2
+        nmap -sL -n ${1} | grep 'Nmap scan' | cut -d ' ' -f 5
     fi;
