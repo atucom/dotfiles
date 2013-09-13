07e8622872e507a565fa8bb88ecc15c4158ec1c0
atucom <log1kl@gmail.com>
2013-09-13 15:06:03 -0700

    fixed mountshare

--- bin/atu-mountshare.sh
+++ bin/atu-mountshare.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #$0 DOMAIN/user%password //10.1.1.1/SYSVOL
-if [[ $1 -z ]]; then
+if [[ -z $1 ]] || [[ -z $2 ]] ; then
 	echo "Mounts a share with given creds"
 	echo " $0 'DOMAIN/user%password' //IPADDR/SHARE"
+else
 credentials="$1" #smbclient/winexe format creds as first arg
 username="$(printf '%s' "$credentials" | cut -d '/' -f2 | cut -d '%' -f1)" #grab the username out of the first arg
 share="$2" #second arg is the share to mount
@@ -14,3 +15,4 @@ if $? ; then
 	else
 		echo "Something went wrong"
 	fi
+fi
