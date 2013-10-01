ca7bdbc5357ee27a1de803306f2c3040cde55f0f
atucom <log1kl@gmail.com>
2013-10-01 15:30:06 -0500

    added the ip info script

---
+++ bin/atu-ipinfo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#get default networking details
+
+hname="$(hostname)"
+default_int="$(ip r g 8.8.8.8 | grep dev | awk '{print $5}')"
+default_ip="$(ip r g 8.8.8.8 | grep dev | awk '{print $7}')"
+default_gw="$(ip r g 8.8.8.8 | grep dev | awk '{print $3}')"
+default_nw="$(ip a sh "$default_int" | grep 'inet ' | awk '{print $2}')"
+default_ns="$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}' | tr '\n' ', ')"
+printf 'Hostname: %s\n' "$hname"
+printf 'Intface: %s\n' "$default_int"
+printf 'IP Addr: %s\n' "$default_ip"
+printf 'Gateway: %s\n' "$default_gw"
+printf 'Network: %s\n' "$default_nw"
+printf 'DNS: %s\n' "$default_ns"
