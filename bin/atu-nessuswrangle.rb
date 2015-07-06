531748defb1183252e01172d01d99e71582921d1
atucom <log1kl@gmail.com>
2015-07-06 16:16:29 -0700

    redid nessuswrangle to be easy to load into other scripts

--- bin/atu-nessuswrangle.rb
+++ bin/atu-nessuswrangle.rb
@@ -12,31 +12,38 @@
       exit
     end
   }
-#parse command line options
-x=nil
-options = {} #hash that hold the options
-o=OptionParser.new do |opts|
-   opts.banner = "Usage: #{$0} [options] ..." #the banner to display at the top
-   optparse = OptionParser.new do|opts| #start defining options below
-   opts.on( '-f', '--file File', 'The .nessus file to read from' ) {|i| options[:file] = i}
-   opts.on( '-s', '--string asdf', 'The string to search for' ) {|i| options[:string] = i}
-   opts.on( '-h', '--help', 'Display this screen' ) {puts opts; exit}
-   x=opts
-  end.parse!(ARGV)
-end
-if options[:file].nil?
- puts x
- exit
+
+
+
+def search_nessus_file(nessusfile, search_string)
+  nessusfile = File.open(nessusfile)
+  Nessus::Parse.new(nessusfile) do |scan|
+      scan.each_host do |host|
+          host.each_event do |event|
+              if event.name =~/#{search_string}/
+                  puts "#{event.name},#{host.ip},#{event.port.number}"#event.output,event.risk,event.severity
+              end
+          end
+      end
+  end
 end
 
-nessusfile = File.open(options[:file])
-Nessus::Parse.new(nessusfile) do |scan|
-    scan.each_host do |host|
-        host.each_event do |event|
-            if event.name =~/#{options[:string]}/
-                puts "#{event.name},#{host.ip},#{event.port.number}"
-            end
-        end
-    end
+if __FILE__ == $0
+  #parse command line options
+  x=nil
+  options = {} #hash that hold the options
+  o=OptionParser.new do |opts|
+     opts.banner = "Usage: #{$0} [options] ..." #the banner to display at the top
+     optparse = OptionParser.new do|opts| #start defining options below
+     opts.on( '-f', '--file File', 'The .nessus file to read from' ) {|i| options[:file] = i}
+     opts.on( '-s', '--string asdf', 'The string to search for' ) {|i| options[:string] = i}
+     opts.on( '-h', '--help', 'Display this screen' ) {puts opts; exit}
+     x=opts
+    end.parse!(ARGV)
+  end
+  if options[:file].nil?
+   puts x
+   exit
+  end
+  search_nessus_file(options[:file], options[:string])
 end
-
