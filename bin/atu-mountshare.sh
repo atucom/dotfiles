b8281de58564438f1878d9ea2ea250d3569c69be
atucom <log1kl@gmail.com>
2016-01-05 21:59:46 -0600

    added mount.cifs check for atu-mountshare

--- bin/atu-mountshare.sh
+++ bin/atu-mountshare.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #$0 DOMAIN/user%password //10.1.1.1/SYSVOL
+#if the mounting fails, make sure you have cifs-utils install from apt-get. Kali2 doesnt seem to have it installed by default
 if [[ -z $1 ]] || [[ -z $2 ]] ; then
 	echo "Mounts a share with given creds"
 	echo " $0 'DOMAIN/user%password' //HOST/SHARE"
 else
+  if [[ $(which mount.cifs > /dev/null; echo $?) == "1" ]] ; then
+    echo "You dont have mount.cifs installed, install the cifs-utils package"
+    exit 1
+  fi
 	credentials="$1" #smbclient/winexe format creds as first arg
 	username="$(printf '%s' "$credentials" | cut -d '/' -f2 | cut -d '%' -f1)" #grab the username out of the first arg
   domain="$(printf '%s' "$credentials" | cut -d '/' -f1)"
@@ -11,7 +16,7 @@ else
 	share="$2" #second arg is the share to mount
 	mountpoint="/mnt/mountshare/${username}/${share}" #default mountpoint
 	mkdir -p "$mountpoint" #create the mountpoint dir
-	mount -t cifs "$share" "$mountpoint" -o user="$user",dom="$domain",pass="$pass" #mount the share
+	mount.cifs "$share" "$mountpoint" -o user="$username",dom="$domain",pass="$pass" #mount the share
 	if [[ $? == "0" ]]; then
 			echo "Mounted to $mountpoint"
 		else
