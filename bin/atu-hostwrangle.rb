bad6b586dfc5acd5630afde846134eb8d4b3f64d
atucom <log1kl@gmail.com>
2016-03-23 14:38:52 -0500

    refactored hostwrangle a little

--- bin/atu-hostwrangle.rb
+++ bin/atu-hostwrangle.rb
@@ -11,34 +11,28 @@
     end
   }
 
-
+def nmapparse(args)
+          Nmap::XML.new(args) do |xml| #take first argument as the xml file
+            xml.each_host do |host| # iterate through all detected hosts in xml file
+              host.each_port do |port| #for every port that is open, output the code block below
+                line={}
+                if port.service.nil?
+                  line[:service] = nil
+                  line[:version] = nil
+                else
+                  line[:service] = port.service.product
+                  line[:version] = port.service.version
+                end
+                puts "#{host.ip},#{port.number},#{host.hostnames.join('|')},#{port.protocol},#{port.state},#{port.service},#{line[:service]},#{line[:version]}"
+              end
+            end
+        end
+end
 if ARGV.empty?
     puts "This script converts an nmap XML output file to easily grep/awkable format"
     puts "\t Usage: #{$0} file1.xml file2.xml file3.xml ... "
 else
     ARGV.each do |args|
-        #puts args
-        Nmap::XML.new(args) do |xml| #take first argument as the xml file
-          xml.each_host do |host| # iterate through all detected hosts in xml file
-            host.each_port do |port| #for every port that is open, output the code block below
-              line={}
-              if port.service.nil?
-                line[:service] = nil
-                line[:version] = nil
-              else
-                line[:service] = port.service.product
-                line[:version] = port.service.version
-              end
-
-              #begin
-              puts "#{host.ip},#{port.number},#{host.hostnames.join('|')},#{port.protocol},#{port.state},#{port.service},#{line[:service]},#{line[:version]}"
-            #rescue NoMethodError
-             # puts "something went wrong on #{host.ip}:#{port.number}"
-             # exit
-            #end
-            end
-          end
-        end
+        nmapparse args
     end
 end
-
