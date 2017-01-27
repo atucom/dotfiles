54e66ac21ebe0282aa708bc829336be8f20cff33
thejosko <jskorich@secureworks.com>
2017-01-27 15:25:34 -0600

    Added Subject Alt Name grab. Moved call to ssl_cert before header print.

--- bin/atu-hostmap.rb
+++ bin/atu-hostmap.rb
@@ -27,10 +27,17 @@ end
 
 puts "#{ip} RESULTS:"
 begin
-print "  SSL - #{ip}:#{port} - "
-cert = ssl_cert(ip,port)
-cert.subject.to_s.scan(/CN=(.*)$/)
-puts "#{$1}"
+  cert = ssl_cert(ip,port) 
+  print "  SSL - #{ip}:#{port} - "
+  cert.subject.to_s.scan(/CN=(.*)$/)
+  puts "#{$1}"
+  #Adding in Subject Alt Name grab from cert
+  subject_alt_name = cert.extensions.find {|e| e.oid == "subjectAltName"}
+  asn_san = OpenSSL::ASN1.decode(subject_alt_name)
+  asn_san_sequence = OpenSSL::ASN1.decode(asn_san.value[1].value)
+  asn_san_sequence.each do |asn_data|
+    puts "  SSL/SAN - #{ip}:#{port} - " + asn_data.value
+  end
 rescue Errno::ETIMEDOUT
   puts "  SSL - #{ip}:#{port} - ERROR: TIME OUT"
 rescue Errno::ECONNREFUSED
